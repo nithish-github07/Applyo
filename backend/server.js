f08dfb025e72ff18f03b82c08f1a50005a769622
nithish-github07 <nithi4527@gmail.com>
2026-06-07 22:30:13 +0530

    Make some changes in the url

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,19 @@ const app = express();
 
 connectDB();
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    process.env.FRONTEND_URL
+].filter(Boolean);
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 app.use(express.json());
