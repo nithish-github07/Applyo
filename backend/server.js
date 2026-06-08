14a611df5a55d38ce3bf068785011e22967ad46a
nithish-github07 <nithi4527@gmail.com>
2026-06-08 20:21:23 +0530

    Change the frontend cors url in the backend

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 connectDB();
 
 const allowedOrigins = [
-    'http://localhost:5173',
+    'https://applyojobs.vercel.app',
     process.env.FRONTEND_URL
 ].filter(Boolean);
 
@@ -32,7 +32,7 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
 const limiter = rateLimit({
@@ -43,22 +43,22 @@ const limiter = rateLimit({
     },
 });
 
-app.use("/api",limiter);
-app.use("/api/auth",authRoutes);
-app.use("/api/jobs",jobRoutes);
-app.use("/api/applications",applicationRoutes);
-app.use("/api/saved-jobs",savedJobRoutes);
+app.use("/api", limiter);
+app.use("/api/auth", authRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/applications", applicationRoutes);
+app.use("/api/saved-jobs", savedJobRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/user",userRoutes);
+app.use("/api/user", userRoutes);
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
     res.send("Job Portal API running...");
 });
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
